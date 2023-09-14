1b269d666f5bd0090e46968d1e3764d878b9794a
Marcus Holtz <130122354+MarcusHoltz@users.noreply.github.com>
2023-09-14 13:40:10 -0600

    ok

--- generate-password.sh
+++ generate-password.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 ########################################################
-#####          What does this script do?           #####
+#####          What does this script do?           #####
 ########################################################
 ## Inspired by: https://github.com/pashword/pashword ##
-## This script hopes to create a hashed password     ##
-## that cannot be found in a rainbow table           ##
+## This script hopes to create a hashed password     ##
+## that cannot be found in a rainbow table           ##
 #######################################################
-## This script requires 270MB of free RAM            ##
-## Be sure you have installed:                       ##
-##          zip, openssl, argon2                     ##
+## This script requires 270MB of free RAM            ##
+## Be sure you have installed:                       ##
+##          zip, openssl, argon2                     ##
 #######################################################
-## What directory do you want to                     ##
-## store your password files in?                     ##
+## What directory do you want to                     ##
+## store your password files in?                     ##
 #######################################################
 PASSWORD_FILES_LOCATION=~/Documents/.passwords
 #######################################################
@@ -19,13 +19,13 @@ PASSWORD_FILES_LOCATION=~/Documents/.passwords
 COMPOOTER_SPEED=$(lscpu | grep -oP "BogoMIPS:\s+\K\w+")
 # Check if COMPOOTER_SPEED is in one of the specified ranges
 if ((COMPOOTER_SPEED >= 0 && COMPOOTER_SPEED <= 5000)); then
-  ENCRYPTION_SPEED=SLOW
+  ENCRYPTION_SPEED=SLOW
 elif ((COMPOOTER_SPEED >= 5001 && COMPOOTER_SPEED <= 7999)); then
-  ENCRYPTION_SPEED=OK
+  ENCRYPTION_SPEED=OK
 elif ((COMPOOTER_SPEED >= 8000 && COMPOOTER_SPEED <= 11000)); then
-  ENCRYPTION_SPEED=GOOD
+  ENCRYPTION_SPEED=GOOD
 else
-  ENCRYPTION_SPEED=GREAT
+  ENCRYPTION_SPEED=GREAT
 fi
 # Define an associative array to map ENCRYPTION_SPEED values to sleep durations
 declare -A sleep_times
@@ -42,20 +42,20 @@ read -s PASWRD1_age
 echo -e "Is this a marketing survey, or a password generator...\n"; sleep .5; echo -e "Next, with the **first letter capitalized**, \nEnter the name of the **Website or Service** you're creating a password for:"
 read -s PASWRD1_service
 echo -e "\nBe sure that first character was a capital letter!"; sleep 1
-echo -e "This script will insert a colon now     :     \n"
+echo -e "This script will insert a colon now     :     \n"
 echo -e "Now enter your username, tied to the service above:"
 read -s PASWRD1_username
 echo -e "Last Step.\n\nEnter a password that you can remember:"
 read -s PASWRD1_password
 echo -e "\nGreat!"; sleep .5; echo -e "We're done entering our information!"
 sleep 1;
-echo -e "\n**********************************************************************\n   Let's pass this data into our hash  (may take up to 30 seconds)\n**********************************************************************"; sleep 1;
+echo -e "\n**********************************************************************\n   Let's pass this data into our hash  (may take up to 30 seconds)\n**********************************************************************"; sleep 1;
 # Make sure that directory exists, or create it
 if [ -d ${PASSWORD_FILES_LOCATION}/.${PASWRD1_phrase} ]
 then
-    echo "I am glad you like this script."
+    echo "I am glad you like this script."
 else
-    mkdir -p ${PASSWORD_FILES_LOCATION}/.${PASWRD1_phrase}
+    mkdir -p ${PASSWORD_FILES_LOCATION}/.${PASWRD1_phrase}
 fi
 # Done with directory concerns
 # Record the variables we need later on
